c90ec285bc6e57681b4d00d6998fcf85e5819325
Maharshi Gor <gor.maharshi@gmail.com>
2025-02-14 15:42:37 -0500

    RF: Doc String added for window file.

--- fury/window.py
+++ fury/window.py
@@ -33,6 +33,19 @@ class Scene(GfxScene):
         skybox=None,
         lights=None,
     ):
+        """Data Structure to arrange the logical and spatial representation of the
+        actors in the graphical scene.
+
+        Parameters
+        ----------
+        background : tuple, optional
+            Uniform color to show in the background of scene, by default (0, 0, 0, 1)
+        skybox : Texture, optional
+            PyGfx Texture object, by default None
+        lights : list, optional
+            list of Light objects to add to the scene. If not passed AmbientLight is
+            added to the scene, by default None
+        """
         super().__init__()
 
         self._bg_color = background
@@ -53,31 +66,69 @@ class Scene(GfxScene):
         self.add(*self.lights)
 
     def _skybox(self, cube_map):
+        """Create skybox background from cubemap.
+
+        Parameters
+        ----------
+        cube_map : Texture
+            PyGfx Texture object
+
+        Returns
+        -------
+        Background
+            PyGfx background object
+        """
         return Background(
             geometry=None, material=BackgroundSkyboxMaterial(map=cube_map)
         )
 
     @property
     def background(self):
+        """Get background Color of the scene.
+
+        Returns
+        -------
+        tuple
+            (R, G, B, A) tuple
+        """
         return self._background_color
 
     @background.setter
     def background(self, value):
+        """Set background color of the scene.
+
+        Parameters
+        ----------
+        value : tuple
+            (R, G, B, A) tuple
+        """
         self.remove(self._bg_actor)
         self._bg_actor = Background.from_color(value)
         self.add(self._bg_actor)
 
     def set_skybox(self, cube_map):
+        """Set skybox from cubemap as background.
+
+        Parameters
+        ----------
+        cube_map : Texture
+            PyGfx Texture object
+        """
         self.remove(self._bg_actor)
         self._bg_actor = self._skybox(cube_map)
         self.add(self._bg_actor)
 
     def clear(self):
+        """Removes all the children from the scene graph."""
         self.remove(*self.children)
 
 
 @dataclass
 class Screen:
+    """Define an independent viewport to show in the window, it holds a scene graph to
+    show actors in the defined space.
+    """
+
     viewport: Viewport
     scene: Scene
     camera: Camera
@@ -85,24 +136,76 @@ class Screen:
 
     @property
     def size(self):
+        """Size of the screen.
+
+        Returns
+        -------
+        tuple
+            (w, h)
+        """
         return self.viewport.rect[2:]
 
     @property
     def position(self):
+        """Position of the screen in the window.
+
+        Returns
+        -------
+        tuple
+            (x, y)
+        """
         return self.viewport.rect[:2]
 
     @property
     def bounding_box(self):
+        """Bounding box of the screen in the window.
+
+        Returns
+        -------
+        tuple
+            (x, y, w, h)
+        """
         return self.viewport.rect
 
     @bounding_box.setter
     def bounding_box(self, value):
+        """Set bounding box of the screen in the window.
+
+        Parameters
+        ----------
+        value : tuple
+            (x, y, w, h)
+        """
         self.viewport.rect = value
 
 
 def create_screen(
     renderer, *, rect=None, scene=None, camera=None, controller=None, camera_light=True
 ):
+    """Compose a screen.
+
+    Parameters
+    ----------
+    renderer : Renderer
+        PyGfx Renderer object to hold the viewport of the screen
+    rect : tuple, optional
+        Bounding box of (x, y, w, h), by default None
+    scene : Scene, optional
+        scene graph of the screen. If None, a new scene is created, by default None
+    camera : Camera, optional
+        PyGfx camera of choice to visualize. If None, Perspective Camera is used, by
+        default None
+    controller : Controller, optional
+        PyGfx Controller of choice to visualize. If None, Orbit Controller is used, by
+        default None
+    camera_light : bool, optional
+        If True a directional light is attached on top of the camera, by default True
+
+    Returns
+    -------
+    Screen
+        screen object to render.
+    """
     vp = Viewport(renderer, rect)
     if scene is None:
         scene = Scene()
@@ -122,6 +225,17 @@ def create_screen(
 
 
 def update_camera(camera, size, target):
+    """Updates the camera to face the target.
+
+    Parameters
+    ----------
+    camera : Camera
+        PyGfx camera object to face the target
+    size : tuple
+        Size of the target object
+    target : Object
+        PyGfx Object to show on camera.
+    """
     camera.width = size[0]
     camera.height = size[1]
 
@@ -132,12 +246,30 @@ def update_camera(camera, size, target):
 
 
 def update_viewports(screens, screen_bbs):
+    """Update the screen viewports to given bounding boxes.
+
+    Parameters
+    ----------
+    screens : list
+        list of Screen objects
+    screen_bbs : list
+        list of tuple of bounding boxes
+    """
     for screen, screen_bb in zip(screens, screen_bbs):
         screen.bounding_box = screen_bb
         update_camera(screen.camera, screen.size, screen.scene)
 
 
 def render_screens(renderer, screens):
+    """Render screens in renderer on update.
+
+    Parameters
+    ----------
+    renderer : Renderer
+        PyGfx Renderer to update
+    screens : list
+        list of Screen objects
+    """
     for screen in screens:
         screen.viewport.render(screen.scene, screen.camera, flush=False)
 
@@ -145,6 +277,22 @@ def render_screens(renderer, screens):
 
 
 def calculate_screen_sizes(screens, size):
+    """Calculate the screen sizes based on the configurations of the screens.
+
+    Parameters
+    ----------
+    screens : list
+        List of screen config. Each entry in the list indicates a vertical section. Each
+        value in the list indicates horizontal sections in the respective vertical
+        section.
+    size : tuple
+        Size of the window
+
+    Returns
+    -------
+    list
+        list of bounding box of each screen.
+    """
     if screens is None:
         return [(0, 0, *size)]
 
@@ -180,12 +328,6 @@ class ShowManager:
         controller=None,
         title="FURY 2.0",
         size=(800, 800),
-        png_magnify=1,
-        order_transparent=False,
-        stereo="off",
-        multi_samples=8,
-        max_peels=4,
-        occlusion_ratio=0.0,
         blend_mode="weighted_plus",
         window_type="auto",
         pixel_ratio=1,
@@ -195,6 +337,77 @@ class ShowManager:
         qt_app=None,
         qt_parent=None,
     ):
+        """Show manager for the rendering window.
+
+        Parameters
+        ----------
+        renderer : Renderer, optional
+            PyGfx Renderer object. If None, a new Renderer object is created,
+            by default None
+        scene : Scene or list, optional
+            If scene is passed same scene is applied to all the screens.
+            Else each scene is provided to respective screen from the list.
+            If None, a new scene object is created, by default None
+        camera : Camera or list, optional
+            If camera is passed same camera is applied to all the screens.
+            Else each camera is provided to respective screen from the list.
+            If None, a new camera object is created, by default None
+        controller : Controller or list, optional
+            If controller is passed same controller is applied to all the screens.
+            Else each controller is provided to respective screen from the list.
+            If None, a new controller object is created, by default None
+        title : str, optional
+            Title of the window, by default "FURY 2.0"
+        size : tuple, optional
+            Size of the window, by default (800, 800)
+        blend_mode : str, optional
+            Renderer blend mode. One of the following blend mode is accepted
+            * "additive": single-pass approach that adds fragments together.
+            * "opaque": single-pass approach that ignores transparency.
+            * "ordered1": single-pass approach that blends fragments (using alpha
+            blending). Can only produce correct results if fragments are drawn
+            from back to front.
+            * "ordered2": two-pass approach that first processes all opaque
+            fragments and then blends transparent fragments (using alpha blending)
+            with depth-write disabled. The visual results are usually better than
+            ordered1, but still depend on the drawing order.
+            * "weighted": two-pass approach for order independent transparency based
+            on alpha weights.
+            * "weighted_depth": two-pass approach for order independent transparency
+            based on alpha weights and depth [1]. Note that the depth range
+            affects the (quality of the) visual result.
+            * "weighted_plus": three-pass approach for order independent
+            transparency, in which the front-most transparent layer is rendered
+            correctly, while transparent layers behind it are blended using alpha
+            weights, by default "weighted_plus"
+        window_type : str, optional
+            Type of the window. One of the following window type is accepted
+            *auto or glfw: select default GLFW canvas window.
+            *qt: select Qt canvas window.
+            *jupyter: select jupyter_rfb canvas widget.
+            *offscreen: select offscreen canvas to not show any window for remote runs,
+            by default "auto"
+        pixel_ratio : float, optional
+            The ratio between the number of pixels in the render buffer versus the
+            number of pixels in the display buffer. If None, this will be 1 for high-res
+            canvases and 2 otherwise. If greater than 1, SSAA (supersampling
+            anti-aliasing) is applied while converting a render buffer to a display
+            buffer. If smaller than 1, pixels from the render buffer are replicated
+            while converting to a display buffer. This has positive performance
+            implications., by default 1
+        camera_light : bool, optional
+            To attach a light on top of camera, by default True
+        screen_config : list, optional
+            List of all the vertical and horizontal section or list of all the bounding
+            boxes of the screens. If None, single screen is assumed, by default None
+        enable_events : bool, optional
+            Enable the events from mouse and keyboard on the visualization
+            , by default True
+        qt_app : QApplication, optional
+            QtWidgets QApplication object for QtCanvas, by default None
+        qt_parent : QWidget, optional
+            QWidget object for putting the window in a QLayout, by default None
+        """
         self.size = size
         self._title = title
         self._is_qt = False
@@ -221,14 +434,21 @@ class ShowManager:
 
         self.enable_events = enable_events
 
-        self.png_magnify = png_magnify
-        self.order_transparent = order_transparent
-        self.stereo = stereo
         self.timers = []
         self._fps = 0
         self._last_render_time = 0
 
     def _screen_setup(self, scene, camera, controller, camera_light):
+        """Setup to create the screens.
+
+        Parameters
+        ----------
+        scene : Scene
+        camera : Camera
+        controller : Controller
+        camera_light : bool
+            If True, attach a direction light with the camera
+        """
         self._scene = scene
         if not isinstance(scene, list):
             self._scene = [scene] * self._total_screens
@@ -246,8 +466,19 @@ class ShowManager:
             self._camera_light = [camera_light] * self._total_screens
 
     def _setup_window(self, window_type):
+        """Initialize the canvas window.
+
+        Parameters
+        ----------
+        window_type : str
+            Type of the window. One of the following window type is accepted
+            *auto or glfw: select default GLFW canvas window.
+            *qt: select Qt canvas window.
+            *jupyter: select jupyter_rfb canvas widget.
+            *offscreen: select offscreen canvas to not show any window for remote runs.
+        """
         window_type = window_type.lower()
-        if window_type == "auto":
+        if window_type == "auto" or window_type == "glfw":
             self.window = Canvas(size=self.size, title=self._title)
         elif window_type == "qt":
             if self._qt_app is None:
@@ -264,6 +495,7 @@ class ShowManager:
             self.window = Canvas(size=self.size, title=self._title)
 
     def _calculate_total_screens(self):
+        """Calculate the total screens from the screen configurations."""
         if self._screen_config is None:
             self._total_screens = 1
         elif isinstance(self._screen_config[0], int):
@@ -272,6 +504,13 @@ class ShowManager:
             self._total_screens = len(self._screen_config)
 
     def _create_screens(self):
+        """Create screens from screen setup.
+
+        Returns
+        -------
+        list
+            list of Screen objects
+        """
         screens = []
         for i in range(self._total_screens):
             screens.append(
@@ -287,31 +526,63 @@ class ShowManager:
 
     @property
     def app(self):
+        """QtApplication
+
+        Returns
+        -------
+        QtApplication or None
+            If window type is qt returns QtApplication object else returns None.
+        """
         return self._qt_app
 
     @property
     def title(self):
+        """Title of the window.
+
+        Returns
+        -------
+        str
+        """
         return self._title
 
     @title.setter
     def title(self, value):
+        """Set title of the window.
+
+        Parameters
+        ----------
+        value : str
+        """
         self._title = value
         self.window.set_title(self._title)
 
     @property
     def pixel_ratio(self):
+        """Pixel ratio of the renderer.
+
+        Returns
+        -------
+        float
+        """
         return self.renderer.pixel_ratio
 
     @pixel_ratio.setter
     def pixel_ratio(self, value):
+        """Set pixel ratio of the renderer.
+
+        Parameters
+        ----------
+        value : float
+        """
         self.renderer.pixel_ratio = value
 
-    @property
-    def enable_events(self):
-        return self._enable_events
+    def set_enable_events(self, value):
+        """Enable or disables the events on the rendering window.
 
-    @enable_events.setter
-    def enable_events(self, value):
+        Parameters
+        ----------
+        value : bool
+        """
         self._enable_events = value
         if value:
             self.renderer.enable_events()
@@ -321,15 +592,29 @@ class ShowManager:
             s.controller.enabled = value
 
     def snapshot(self, fname):
+        """Save a copy of the rasterized image of the scene(s).
+
+        Parameters
+        ----------
+        fname : str
+            file name or path to store the file
+
+        Returns
+        -------
+        narray
+            numpy array of the image.
+        """
         arr = np.asarray(self.renderer.snapshot())
         img = image_from_array(arr)
         img.save(fname)
         return arr
 
     def render(self):
+        """Rasterize the scene(s)."""
         self.window.request_draw(lambda: render_screens(self.renderer, self.screens))
 
     def start(self):
+        """Start the visualization using show manager."""
         self.render()
         if self._is_qt:
             self._qt_app.exec()
@@ -337,6 +622,13 @@ class ShowManager:
             run()
 
     def resize(self, _event):
+        """Resize the inner screens based on the event on the window.
+
+        Parameters
+        ----------
+        _event : Event
+            PyGfx Event object for window.
+        """
         update_viewports(
             self.screens,
             calculate_screen_sizes(self._screen_config, self.renderer.logical_size),
@@ -352,6 +644,28 @@ def snapshot(
     actors=None,
     return_array=False,
 ):
+    """Save a snapshot of the rasterized image of the window.
+
+    Parameters
+    ----------
+    scene : Scene or list, optional
+        scene(s) graph to capture the image, by default None
+    screen_config : list, optional
+        List of all the vertical and horizontal section or list of all the bounding
+        boxes of the screens. If None, single screen is assumed, by default None
+    fname : str, optional
+        Name or path of the output image file, by default "output.png"
+    actors : Object, optional
+        PyGfx Objects to show on the scene. Works with single scene configuration,
+        by default None
+    return_array : bool, optional
+        If True, return the numpy array of the image, by default False
+
+    Returns
+    -------
+    narray
+        numpy array of the image
+    """
     if actors is not None:
         scene = Scene()
         scene.add(*actors)
@@ -368,6 +682,20 @@ def snapshot(
 
 
 def display(actors, *, window_type="auto"):
+    """Display given actors in a fury window. A Quick way to visualize the actors.
+
+    Parameters
+    ----------
+    actors : Object
+        PyGfx Object
+    window_type : str, optional
+        Type of the window. One of the following window type is accepted
+        *auto or glfw: select default GLFW canvas window.
+        *qt: select Qt canvas window.
+        *jupyter: select jupyter_rfb canvas widget.
+        *offscreen: select offscreen canvas to not show any window for remote runs,
+        by default "auto"
+    """
     scene = Scene()
     scene.add(*actors)
     show_m = ShowManager(scene=scene, window_type=window_type)
